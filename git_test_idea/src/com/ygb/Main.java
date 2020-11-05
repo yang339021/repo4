c115b0123eacf6499eedaaf2c37289bbc919c0ee
Yang339021 <Yang339021@163.com>
2020-11-05 18:05:59 +0800

    新建文件

---
+++ git_test_idea/src/com/ygb/Main.java
@@ -0,0 +1,13 @@
+package com.ygb;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("jfklsdjlffdjskl");
+        System.out.println("jfklsdjlffdjskl");
+        System.out.println("jfklsdjlffdjskl");
+        System.out.println("jfklsdjlffdjskl");
+
+    }
+}
